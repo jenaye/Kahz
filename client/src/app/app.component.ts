c8221a5d0640dabac97fb3c0165552544f1a107b
Arnaud POINTET <arnaud.pointet@gmail.com>
2017-10-05 10:44:40 +0200

    fix socket

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import * as io from "socket.io-client";
 
 @Component({
@@ -8,28 +8,25 @@ import * as io from "socket.io-client";
 })
 export class AppComponent {
   private message: string;
-  private AllMessage: any;
-  private TouslesMessages:any;
+  private TouslesMessages: any[];
   title = 'Kahz';
   socket = io('http://localhost:8000');
 
   /*
   init on recupere les messages du tableau
    */
-    ngOnInit() {
-          this.socket.on('ShowMessage', data =>{
-              this.TouslesMessages = data;
-              console.log(data)
-          })
-    }
+  ngOnInit() {
+    this.TouslesMessages = [];
+    this.socket.on('ShowMessage', data => {
+      this.TouslesMessages = data.TouslesMessages;
+    })
+  }
 
-    send(){
-      this.socket.emit('new', { "message": this.message, "userid ": new Date() })
-        this.socket.on('ShowMessage', data =>{
-            this.TouslesMessages = data;
-            console.log(data)
-        })
-    }
+  send() {
+    let data = {"message": this.message, "userid ": new Date()}
+    this.socket.emit('new', data)
+    this.TouslesMessages.push(data)
+  }
 
 
 }
