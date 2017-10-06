5f67c354cfc9037646c97799bd48a63ea534dc4a
Houziaux mike <Stahlilistenvoid@gmail.com>
2017-10-06 10:09:01 +0200

    start notificatioon

--- server/server.js
+++ server/server.js
@@ -7,15 +7,12 @@ on watch les connexions
  */
 socket.on('connection', (client) => {
     console.log('ID de l\'utilisateur connecter =>', client.id)
-    /*
-    quand l'user envoi un message on recupere et on stock dans un tableau
-     */
     client.on('newMessage', data => {
         AllMessage.push(data)
         client.broadcast.emit('ShowMessage', { TouslesMessages: AllMessage } )
     })
     client.emit('userid', { userid : client.id});
-  //   socket.broadcast.emit('new-connection', { "NewUser": client.id }  ) 
+    client.broadcast.emit('new-connection', { "NewUser": client.id }  )
 })
 
 
