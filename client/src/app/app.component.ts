4308a9e9cbe93add536ed4d7f35aeeee0c4b97ab
Houziaux mike <Stahlilistenvoid@gmail.com>
2017-10-13 12:00:49 +0200

    add logout sync with socket

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent implements OnInit, AfterViewChecked {
   private message: string;
   private TouslesMessages: any[];
   private userid: string;
+  private counter: any;
 
   title = 'Kahz';
   socket = io('http://localhost:8000');
@@ -25,6 +26,13 @@ export class AppComponent implements OnInit, AfterViewChecked {
       })
       this.socket.on('new-connection', data => {
         alert('les gars il y a un nouveau mec qui est la' + data.NewUser)
+      })
+      this.socket.on('counter', data => {
+          this.counter = data.counter;
+      })
+      this.socket.on('LogoutSession', data => {
+              alert(data.logoutMessage)
+
       })
     this.scrollToBottom();
   }
--- server/server.js
+++ server/server.js
@@ -6,13 +6,18 @@ const AllMessage = []
 on watch les connexions
  */
 socket.on('connection', (client) => {
-    console.log('ID de l\'utilisateur connecter =>', client.id)
+    console.log('New session =>', client.id)
     client.on('newMessage', data => {
         AllMessage.push(data)
         client.broadcast.emit('ShowMessage', { TouslesMessages: AllMessage } )
     })
     client.emit('userid', { userid : client.id});
+    client.emit('counter', {'counter': socket.engine.clientsCount })
     client.broadcast.emit('new-connection', { "NewUser": client.id }  )
+    client.on('disconnect', function()  {
+        console.log('Socket disconnected !',client.id);
+        client.broadcast.emit('LogoutSession', { 'logoutMessage': 'un utilisateur vient de se deconnecter' }  )
+    });
 })
 
 
